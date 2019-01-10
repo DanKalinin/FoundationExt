3bd509e1a0567b6abd4a512c10bf70be11048436
Dan Kalinin <daniil551@hotmail.com>
2019-01-10 20:50:46 +0300

    FoundationExt

--- FoundationExt/FoundationExt.h
+++ FoundationExt/FoundationExt.h
@@ -10,3 +10,16 @@
 
 FOUNDATION_EXPORT double FoundationExtVersionNumber;
 FOUNDATION_EXPORT const unsigned char FoundationExtVersionString[];
+
+#import <FoundationExt/NSEOperation.h>
+#import <FoundationExt/NSEObject.h>
+#import <FoundationExt/NSEArray.h>
+#import <FoundationExt/NSEOrderedSet.h>
+#import <FoundationExt/NSEDictionary.h>
+#import <FoundationExt/NSEDictionaryObject.h>
+#import <FoundationExt/NSEError.h>
+#import <FoundationExt/NSEInputStream.h>
+
+// Deprecated
+
+#import <FoundationExt/Main.h>
