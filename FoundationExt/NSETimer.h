ba00f46beb78c13d82833eddf9193c3adaec99b3
Dan Kalinin <daniil551@hotmail.com>
2019-01-16 13:57:58 +0300

    Timer

---
+++ FoundationExt/NSETimer.h
@@ -0,0 +1,16 @@
+//
+//  NSETimer.h
+//  FoundationExt
+//
+//  Created by Dan Kalinin on 1/16/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSETimer : NSTimer
+
+@end
+
+NS_ASSUME_NONNULL_END
