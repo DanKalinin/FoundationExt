77f778f9036e7fd68fe659579a55b8f43ef980f7
Dan Kalinin <daniil551@hotmail.com>
2019-12-11 14:08:41 +0300

    Header

--- FoundationExt/FoundationExt.h
+++ FoundationExt/FoundationExt.h
@@ -11,22 +11,22 @@
 FOUNDATION_EXPORT double FoundationExtVersionNumber;
 FOUNDATION_EXPORT const unsigned char FoundationExtVersionString[];
 
-#import <FoundationExt/NSEMain.h>
-#import <FoundationExt/NSEObject.h>
-#import <FoundationExt/NSEOperation.h>
-#import <FoundationExt/NSEArray.h>
-#import <FoundationExt/NSEOrderedSet.h>
-#import <FoundationExt/NSEDictionary.h>
-#import <FoundationExt/NSEDictionaryObject.h>
-#import <FoundationExt/NSEThread.h>
-#import <FoundationExt/NSEStream.h>
-#import <FoundationExt/NSEInputStream.h>
-#import <FoundationExt/NSEOutputStream.h>
-#import <FoundationExt/NSEStreams.h>
-#import <FoundationExt/NSETimer.h>
-#import <FoundationExt/NSEClock.h>
-#import <FoundationExt/NSERPC.h>
-#import <FoundationExt/NSENetServiceBrowser.h>
+//#import <FoundationExt/NSEMain.h>
+//#import <FoundationExt/NSEObject.h>
+//#import <FoundationExt/NSEOperation.h>
+//#import <FoundationExt/NSEArray.h>
+//#import <FoundationExt/NSEOrderedSet.h>
+//#import <FoundationExt/NSEDictionary.h>
+//#import <FoundationExt/NSEDictionaryObject.h>
+//#import <FoundationExt/NSEThread.h>
+//#import <FoundationExt/NSEStream.h>
+//#import <FoundationExt/NSEInputStream.h>
+//#import <FoundationExt/NSEOutputStream.h>
+//#import <FoundationExt/NSEStreams.h>
+//#import <FoundationExt/NSETimer.h>
+//#import <FoundationExt/NSEClock.h>
+//#import <FoundationExt/NSERPC.h>
+//#import <FoundationExt/NSENetServiceBrowser.h>
 
 // Deprecated
 
